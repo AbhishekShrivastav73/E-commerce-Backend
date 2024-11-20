eb7367ac9bcce939c3dabf368063d01dc274e702
AbhishekShrivastav73 <abhishekshrivastav2121@gmail.com>
2024-11-20 23:26:59 +0530

    multer

--- app.js
+++ app.js
@@ -1,28 +1,28 @@
 const express = require("express");
-const fileUpload = require('express-fileupload');
-const cors = require('cors')
+const fileUpload = require("express-fileupload");
+const cors = require("cors");
 const app = express();
 require("dotenv").config();
 const port = 3000;
 const mongooseConnection = require("./config/mongoose");
-const userRoutes = require('./routes/user.routes')
-const indexRoutes = require('./routes/index.routes')
-const productRoutes = require('./routes/products.routes')
-const cookieParser = require('cookie-parser');
+const userRoutes = require("./routes/user.routes");
+const indexRoutes = require("./routes/index.routes");
+const productRoutes = require("./routes/products.routes");
+const cookieParser = require("cookie-parser");
+const path = require("path");
 
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 // eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NzI4NzdjZTcyMzhlNTNiN2Y4NzMxOTEiLCJpYXQiOjE3MzA3MDUzNTksImV4cCI6MTczMDcwODk1OX0.UM-sd_RsCd9MYL5k0NI-zxW30qRms4S0K6lRzGS23AE
-app.use(cors())
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser())
+app.use(cookieParser());
 // app.use(fileUpload())
 
-
-
-app.use('/',indexRoutes)
-app.use('/users',userRoutes)
-app.use('/product', productRoutes);
+app.use("/", indexRoutes);
+app.use("/users", userRoutes);
+app.use("/product", productRoutes);
 
 app.listen(process.env.PORT, () => {
   console.log(`Server started on port ${port}`);
